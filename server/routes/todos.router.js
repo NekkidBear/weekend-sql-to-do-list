6179b6ea9233d8aae524927b5cafd6da2e6759f3
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-12 19:55:17 -0500

    creating get route

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -3,6 +3,18 @@ const pool = require('../modules/pool');
 
 // POST Route --- CREATE
 // GET Route --- READ
+router.get('/', (req, res) =>{
+    const queryText = `SELECT * FROM "todos";`;
+    pool.query(queryText)
+    .then((result) => {
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log('error in GET', error);
+        res.sendStatus(500);
+    });
+})
+
 // PUT Route --- UPDATE
 // DELETE Route --- DELETE
 

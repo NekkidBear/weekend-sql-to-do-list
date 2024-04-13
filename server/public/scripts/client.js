b55db5393382c87f8f53eec2160bfaf6a17137e0
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-13 11:06:59 -0500

    add logic to indicate completion status

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,12 +7,18 @@ function getAndRenderTodoList(){
     })
     .then(response =>{
         let taskList = response.data;
+        let isComplete = ""
         let tableLocation = document.getElementById("taskListBody")
         for (let task of taskList){
+            if (task.isComplete === true){
+                isComplete = "Completed"
+            } else {
+                isComplete = "In Progress"
+            }
             tableLocation.innerHTML += `
                 <tr>
                     <td>${task.text}</td>
-                    <td>${task.isComplete}</td>
+                    <td>${isComplete}</td>
                     <td><button class ="markCompleteBtn" onclick = "markComplete(${task.id}) role = "button" aria-label = "mark task as complete">✅</button></td>
                     <td><button class = "deleteItemBtn" onclick = "deleteItem(${task.id}) role = "button" aria-label = "delete task">❌</button></td>
                 </tr>`

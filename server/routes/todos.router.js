71b940803a2f3b5d9e907367b8d091c1b9a0078f
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-13 10:05:52 -0500

    fix PUT and POST routes, add HTML Form, add test IDS to inputs and buttons

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -3,12 +3,14 @@ const pool = require('../modules/pool');
 
 // POST Route --- CREATE
 router.post(`/`, (req, res) => {
+    console.log('POST req.body: ', req.body);
     const todoText = req.body.text;
-    const isComplete = req.body.isComplete
-    const sqlText = `INSERT INTO "todos" ("task", "completed")
-    VALUES ($1, $2);`;
+    const isComplete = req.body.isComplete || false //set value to false if it's not provided
+    const sqlText = `
+        INSERT INTO "todos" ("text", "isComplete")
+            VALUES ($1, $2);`;
     const sqlValues = [todoText, isComplete];
-
+    console.log(sqlText, sqlValues)
     pool.query(sqlText, sqlValues)
         .then(() =>{
             console.log("item added");
@@ -35,21 +37,23 @@ router.get('/', (req, res) =>{
 
 // PUT Route --- UPDATE
 router.put('/:id', (req, res) => {
+    console.log("PUT req.body: ", req.body);
+    console.log('put req.params.id: ', req.params.id)
     let id = req.params.id;
-
+    let isComplete = true; // set flag to True
     const sqlText = `
         UPDATE todos
-            SET isComplete = true
-            WHERE id = $1;
+            SET "isComplete" = $1
+            WHERE id = $2;
     `;
 
-    const sqlValues = [id];
+    const sqlValues = [isComplete, id];
     pool.query(sqlText, sqlValues)
         .then(() =>{ 
             res.sendStatus(200);
         })
         .catch((error) => {
-            console.log('Error updating completion status')
+            console.log('Error updating completion status', error);
             res.sendStatus(500);
         });
 

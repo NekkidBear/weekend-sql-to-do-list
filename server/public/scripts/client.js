688b69c11651c25d53109b4003b86509cfe3ebe4
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-13 10:49:33 -0500

    add columns to table; create getAndRender; add accessibility improvements

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,26 @@
 console.log('JS is sourced!');
+
+function getAndRenderTodoList(){
+    axios({
+        method: "GET",
+        URL: "/todos"
+    })
+    .then(response =>{
+        let taskList = response.body;
+        let tableLocation = document.getElementById("taskListBody")
+        for (let task of taskList){
+            tableLocation.innerHTML = `
+                <tr>
+                    <td>${task.text}</td>
+                    <td>${task.isComplete}</td>
+                    <td><button class ="markCompleteBtn" onclick = "markComplete(${task.id}) role = "button" aria-label = "mark task as complete">✅</button></td>
+                    <td><button class = "deleteItemBtn" conclick = "deleteItem(${task.id}) role = "button" aria-label = "delete task">❌</button></td>
+                </tr>`
+        }
+    })
+    .catch(error => {
+        console.error('error fetching todos:', error);
+    });
+}
+
+getAndRenderTodoList()

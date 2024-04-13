246c63bed1c7878a5b6349daa481cbf85cf8c83b
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-13 10:57:48 -0500

    correct typos in axios call

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,18 +3,18 @@ console.log('JS is sourced!');
 function getAndRenderTodoList(){
     axios({
         method: "GET",
-        URL: "/todos"
+        url: "/todos"
     })
     .then(response =>{
-        let taskList = response.body;
+        let taskList = response.data;
         let tableLocation = document.getElementById("taskListBody")
         for (let task of taskList){
-            tableLocation.innerHTML = `
+            tableLocation.innerHTML += `
                 <tr>
                     <td>${task.text}</td>
                     <td>${task.isComplete}</td>
                     <td><button class ="markCompleteBtn" onclick = "markComplete(${task.id}) role = "button" aria-label = "mark task as complete">✅</button></td>
-                    <td><button class = "deleteItemBtn" conclick = "deleteItem(${task.id}) role = "button" aria-label = "delete task">❌</button></td>
+                    <td><button class = "deleteItemBtn" onclick = "deleteItem(${task.id}) role = "button" aria-label = "delete task">❌</button></td>
                 </tr>`
         }
     })
